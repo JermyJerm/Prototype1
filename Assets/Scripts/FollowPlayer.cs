15fcac34c5f2c4df03f3c27653871ac6787e24a8
JermyJerm <99265510+JermyJerm@users.noreply.github.com>
2022-12-15 12:08:34 -0500

    progress

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -12,7 +12,6 @@ public class FollowPlayer : MonoBehaviour
         
     }
     
-    
     void Update()
     {
         transform.position = player.transform.position + offset;
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject Car;
+    private Vector3 spawnPos = new Vector3(0, 13, 120);
+    private float startDelay = 2;
+    private float repeatRate = 4;
+    private PlayerController playerControllerScript;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
